b6d015b8d49336a0cac3a665b77f313b83c1bc8f
pausalazar101 <123917747+pausalazar101@users.noreply.github.com>
2023-05-20 15:42:48 -0500

    Update LAB_SQL_self_cross_join.sql

--- LAB_SQL_self_cross_join.sql
+++ LAB_SQL_self_cross_join.sql
@@ -10,15 +10,28 @@ JOIN sakila.film_actor fa2 ON f.film_id = fa2.film_id
 JOIN sakila.actor a2 ON fa2.actor_id = a2.actor_id
 WHERE a1.actor_id < a2.actor_id;
 
-SELECT c1.customer_id AS customer1_id, c1.first_name AS customer1_first_name, c1.last_name AS customer1_last_name,
-       c2.customer_id AS customer2_id, c2.first_name AS customer2_first_name, c2.last_name AS customer2_last_name,
-       r.inventory_id, COUNT(*) AS rental_count
-FROM sakila.rental r
-JOIN sakila.customer c1 ON r.customer_id = c1.customer_id
-JOIN sakila.customer c2 ON r.inventory_id = c2.customer_id
-WHERE c1.customer_id < c2.customer_id
-GROUP BY c1.customer_id, c2.customer_id, r.inventory_id
-HAVING rental_count > 3;
+
+
+SELECT c1.customer_id, c2.customer_id, COUNT(*) AS coincidences
+FROM (
+    SELECT r.customer_id, r.inventory_id, i.film_id, f.title
+    FROM rental r
+    JOIN inventory i ON r.inventory_id = i.inventory_id
+    JOIN film f ON f.film_id = i.film_id
+) AS c1
+JOIN (
+    SELECT r.customer_id, r.inventory_id, i.film_id, f.title
+    FROM rental r
+    JOIN inventory i ON r.inventory_id = i.inventory_id
+    JOIN film f ON f.film_id = i.film_id
+) AS c2 ON c1.film_id = c2.film_id
+WHERE c1.customer_id <> c2.customer_id
+GROUP BY c1.customer_id, c2.customer_id
+HAVING COUNT(*) > 3
+ORDER BY c1.customer_id, c2.customer_id;
+
+
+
 
 SELECT a.actor_id, a.first_name, a.last_name, f.film_id, f.title
 FROM sakila.actor a
